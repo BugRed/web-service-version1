734472af45c9fddd66b8222212e654c828613b65
unknown <o.adrian.rodrigues@gmail.com>
2023-08-15 22:02:27 -0300

    Order implementation

--- src/main/java/com/exampleweb/demo/entities/User.java
+++ src/main/java/com/exampleweb/demo/entities/User.java
@@ -1,23 +1,25 @@
 package com.exampleweb.demo.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
-
-
 
 @Entity
 @Table(name = "tb_user")
-public class User implements Serializable{
-	
+public class User implements Serializable {
 
 	private static final long serialVersionUID = 1L;
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
@@ -26,6 +28,10 @@ public class User implements Serializable{
 	private String fone;
 	private String password;
 	
+	@JsonIgnore
+	@OneToMany(mappedBy = "client")
+	private List<Order> orders = new ArrayList<>();
+
 	public User() {
 	}
 
@@ -35,6 +41,10 @@ public class User implements Serializable{
 		this.email = email;
 		this.fone = fone;
 		this.password = password;
+	}
+
+	public List<Order> getOrders() {
+		return orders;
 	}
 
 	public Long getId() {
@@ -93,9 +103,5 @@ public class User implements Serializable{
 		User other = (User) obj;
 		return Objects.equals(id, other.id);
 	}
-	
-	
 
-	
-	
 }
---
+++ src/main/java/com/exampleweb/demo/repositories/OrderRepository.java
@@ -0,0 +1,9 @@
+package com.exampleweb.demo.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.exampleweb.demo.entities.Order;
+
+public interface OrderRepository extends JpaRepository<Order, Long>{
+
+}
