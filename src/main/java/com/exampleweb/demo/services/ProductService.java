afd6be2e088e3b2ccd9fde9c7f8740b8fc1a839a
BugRed <o.adrian.rodrigues@gmail.com>
2023-10-24 21:25:34 -0300

    POST method add in Product

--- src/main/java/com/exampleweb/demo/services/ProductService.java
+++ src/main/java/com/exampleweb/demo/services/ProductService.java
@@ -9,20 +9,29 @@ import org.springframework.stereotype.Service;
 import com.exampleweb.demo.entities.Product;
 import com.exampleweb.demo.repositories.ProductRepository;
 
+import jakarta.transaction.Transactional;
+
 @Service
 public class ProductService {
 
 	@Autowired
 	private ProductRepository repository;
 
+	@Transactional
 	public List<Product> findAll() {
 		return repository.findAll();
 	}
 
+	@Transactional
 	public Product findById(Long id) {
 		Optional<Product> obj = repository.findById(id);
 		return obj.get();
 
 	}
+
+	@Transactional
+    public Product insert(Product obj) {
+        return repository.save(obj);
+    }
 
 }
